c06edfa40dde1260eefa2fde072451badfd050dd
Sally Diament <sally.barzilai@gmail.com>
2022-06-10 13:35:50 -0300

    tentando arredondar o carrinho de compras

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 // Definindo uma constante do valor total:
 const t = 'total-price';
 
-const round = (num, places) => {
-	if (!("" + num).includes("e")) {
-		return +(Math.round(num + "e+" + places)  + "e-" + places);
-	} else {
-		let arr = ("" + num).split("e");
-		let sig = ""
-		if (+arr[1] + places > 0) {
-			sig = "+";
-		}
-
-		return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + places)) + "e-" + places);
-	}
-}
-
 // Função que cria a imagem do produto com o parametro que é a fonte da imagem:
 const createProductImageElement = (imageSource) => {
   // constante que cria um elemento que é uma img via Dom:
